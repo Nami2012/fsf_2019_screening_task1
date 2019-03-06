d3d08c378c246102feb79955f78ad77ea52f7a31
nami2012 <namithasadeeshbabu@gmail.com>
2019-03-06 21:15:18 +0530

    updated

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -3,4 +3,5 @@ from django.contrib.auth.forms import UserCreationForm
 
 def signup(request):
     form = UserCreationForm()
+    
     return render(request,'users/authenticate/signup.html',{'form':form})
